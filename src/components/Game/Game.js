e730a6a7dd720a670ab20b103e5391f5a79ac739
Thong Ta <thong.ta@bpc.ag>
2025-04-08 18:37:53 +0200

    add game logic to check letters

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { sample, range } from '../../utils';
 import { WORDS } from '../../data';
-import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import GameInput from '../GameInput';
 import GameResults from '../GameResults';
 import Guess from '../Guess';
@@ -27,7 +27,10 @@ function Game() {
   return <>
     <GameResults guesses={guesses} />
     {range(0, NUM_OF_GUESSES_ALLOWED).map(({value}, index) => (
-      <Guess key={index} guess={guesses.length > index ? guesses[index].value : undefined} />
+      <Guess key={index}
+        guess={guesses.length > index ? guesses[index].value : undefined}
+        answer={answer} 
+      />
     ))}
     <GameInput addGuess={addGuess} />
     </>;
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-const Guess = ({guess = []}) => {
+const Guess = ({guess = "", answer}) => {
+  const cellStates = checkGuess(guess, answer);
+  const rowLength = 5;
+
   return <p className="guess">
-    {range(0, 5).map(({value}, index) => (
-        <span className="cell" key={index}>{(guess.length > 0) && guess[index]}</span>
-    ))}
+    {range(0, rowLength).map(({value}, index) => {
+        const classes = cellStates && cellStates.length === rowLength 
+            ? `cell ${cellStates[index].status}`
+            : "cell";
+        return <span 
+            className={classes} 
+            key={index}>
+                {(guess.length > 0) && guess[index]}
+        </span>;
+    })}
   </p>;
 }
 
